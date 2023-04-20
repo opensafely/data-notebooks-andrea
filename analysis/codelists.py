f4f5d815151a20693659eb746536ee7d1e7c259d
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-04-20 14:34:11 +0100

    Deleted old files

--- analysis/codelists.py
+++
@@ -1,5 +0,0 @@
-from cohortextractor import codelist_from_csv
-from config import codelist_path
-
-# Change the path of the codelist to your chosen codelist
-codelist = codelist_from_csv(codelist_path, system="snomed")
